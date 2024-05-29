732f76bcc16b01975b60a778a8028791ff36dd21
chadk43 <ckoivuneva43@gmail.com>
2024-05-29 10:26:35 -0600

    add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navigation/Navbar";
 import Footer from "./components/navigation/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,9 @@ export default function RootLayout({ children }) {
     <html lang="en" data-theme="retro">
       <body className={inter.className}>
         <Navbar />
-        <div className="mb-60 sm:mb-0">{children}</div>
+        <div className="mb-60 sm:mb-0">{children}
+        <Analytics/>
+        </div>
 
         <Footer />
       </body>
